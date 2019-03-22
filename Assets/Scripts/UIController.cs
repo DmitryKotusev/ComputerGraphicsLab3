9d9d99de394f34a20a5f6041685d38e1eb6f079b
DmitryKotusev <dssa-1966@yandex.ru>
2019-03-22 23:31:44 +0300

    Initial commit.

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Crosstales.FB;
+
+public class UIController : MonoBehaviour
+{
+    string pathToFile;
+
+    public void ChooseFile()
+    {
+        pathToFile = FileBrowser.OpenSingleFile("Choose file", "", new string[] { "png", "jpg" });
+
+        WWW www = new WWW("file:///" + pathToFile);
+
+        UpdateOriginImage(www.texture);
+        UpdateTransformedImage(www.texture);
+    }
+
+    private void UpdateTransformedImage(Texture2D texture)
+    {
+
+    }
+
+    private void UpdateOriginImage(Texture2D texture)
+    {
+
+    }
+}
