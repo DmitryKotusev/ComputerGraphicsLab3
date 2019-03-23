1b9b1873e5ecca84869444bcd3cd70b721a3179f
DmitryKotusev <dssa-1966@yandex.ru>
2019-03-23 12:40:50 +0300

    Added graysacle prototype.

--- Assets/Scripts/ImageTransfromScript.cs
+++ Assets/Scripts/ImageTransfromScript.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class ImageTransfromScript : MonoBehaviour
 {
-
+    static public Texture2D ConvertToGrayscale(Texture2D texture)
+    {
+        Texture2D resultTexture = new Texture2D(texture.width, texture.height);
+        Color32[] pixels = texture.GetPixels32();
+        for (int x = 0; x < texture.width; x++)
+        {
+            for (int y = 0; y < texture.height; y++)
+            {
+                Color32 pixel = pixels[x + y * texture.width];
+                int p = ((256 * 256 + pixel.r) * 256 + pixel.b) * 256 + pixel.g;
+                int b = p % 256;
+                p = Mathf.FloorToInt(p / 256);
+                int g = p % 256;
+                p = Mathf.FloorToInt(p / 256);
+                int r = p % 256;
+                float l = (0.2126f * r / 255f) + 0.7152f * (g / 255f) + 0.0722f * (b / 255f);
+                Color c = new Color(l, l, l, 1);
+                resultTexture.SetPixel(x, y, c);
+            }
+        }
+        resultTexture.Apply();
+        return resultTexture;
+    }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Crosstales.FB;
+using UnityEditor;
 
 public class UIController : MonoBehaviour
 {
     string pathToFile;
+    public RawImage originalImage;
+    public RawImage transformedImage;
+
+    private void Start()
+    {
+        originalImage.enabled = false;
+        transformedImage.enabled = false;
+        Debug.Log("Red: " + Color.red.grayscale);
+        Debug.Log("Green: " + Color.green.grayscale);
+        Debug.Log("Blue: " + Color.blue.grayscale);
+        Debug.Log("White: " + Color.white.grayscale);
+    }
 
     public void ChooseFile()
     {
+        // pathToFile = EditorUtility.OpenFilePanelWithFilters("Choose file", "", new string[] { "Image files", "png,jpg,jpeg" });
         pathToFile = FileBrowser.OpenSingleFile("Choose file", "", new string[] { "png", "jpg" });
 
         WWW www = new WWW("file:///" + pathToFile);
@@ -19,11 +34,60 @@ public class UIController : MonoBehaviour
 
     private void UpdateTransformedImage(Texture2D texture)
     {
-
+        if(pathToFile != "")
+        {
+            transformedImage.enabled = true;
+            Texture2D grayScaledTexture = ImageTransfromScript.ConvertToGrayscale(texture);
+            transformedImage.texture = grayScaledTexture;
+            transformedImage.SizeToParent();
+        }
+        else
+        {
+            transformedImage.enabled = false;
+        }
     }
 
     private void UpdateOriginImage(Texture2D texture)
     {
+        if (pathToFile != "")
+        {
+            originalImage.enabled = true;
+            Texture2D grayScaledTexture = ImageTransfromScript.ConvertToGrayscale(texture);
+            originalImage.texture = grayScaledTexture;
+            originalImage.SizeToParent();
+        }
+        else
+        {
+            originalImage.enabled = false;
+        }
+    }
+}
 
+static class CanvasExtensions
+{
+    public static Vector2 SizeToParent(this RawImage image, float padding = 0)
+    {
+        var parent = image.transform.parent.GetComponentInParent<RectTransform>();
+        var imageTransform = image.GetComponent<RectTransform>();
+        if (!parent) { return imageTransform.sizeDelta; } //if we don't have a parent, just return our current width; 
+        padding = 1 - padding;
+        float w = 0, h = 0;
+        float ratio = image.texture.width / (float)image.texture.height;
+        var bounds = new Rect(0, 0, parent.rect.width, parent.rect.height);
+        if (Mathf.RoundToInt(imageTransform.eulerAngles.z) % 180 == 90)
+        {
+            //Invert the bounds if the image is rotated 
+            bounds.size = new Vector2(bounds.height, bounds.width);
+        }
+        //Size by height first 
+        h = bounds.height * padding;
+        w = h * ratio;
+        if (w > bounds.width * padding)
+        { //If it doesn't fit, fallback to width; 
+            w = bounds.width * padding;
+            h = w / ratio;
+        }
+        imageTransform.sizeDelta = new Vector2(w, h);
+        return imageTransform.sizeDelta;
     }
 }
